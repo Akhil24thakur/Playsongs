2cee519581346a44f538a39dfb47cd70d7006f24
Akhil Thakur <akhilfromdharampur@gmail.com>
2026-04-03 07:42:13 +0530

    change s_w.js to make app accessible offline

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,79 @@
-let artworkBlob = null;
+const CACHE_NAME = "akhil-music-v1";
 
-const bc = new BroadcastChannel('artwork');
-bc.onmessage = e => { artworkBlob = e.data.blob; };
+const urlsToCache = [
 
-self.addEventListener('fetch', event => {
-  if (event.request.url.includes('/sw-artwork.png')) {
-    event.respondWith(
-      new Response(artworkBlob, { headers: { 'Content-Type': 'image/png' } })
-    );
-    return;
-  }
-  // ... rest of fetch handler
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+
+  "/images/ak01.png",
+
+];
+
+
+/* INSTALL */
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+
+      .then(cache => cache.addAll(urlsToCache))
+
+  );
+
+});
+
+
+/* FETCH */
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+
+      .then(response => {
+
+        return response || fetch(event.request);
+
+      })
+
+  );
+
+});
+
+
+/* UPDATE CACHE */
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(cacheNames => {
+
+      return Promise.all(
+
+        cacheNames.map(cache => {
+
+          if (cache !== CACHE_NAME){
+
+            return caches.delete(cache);
+
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
 });
