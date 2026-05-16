c61585b6d0b251c807a6fe260b4b29f4f6dd1bd8
Akhil Thakur <akhilfromdharampur@gamil.com>
2026-05-16 21:20:16 +0530

    addd tanha

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ if ('serviceWorker' in navigator) {
 
 /* ─── PLAYLIST ──────────────────────────────── */
 const playlist = [
+  { title:'Tanha',           artist:'Akhil', src:'songs/tanha.mp3',           cover:'images/tanha.png'          },
   { title:'For You',           artist:'Akhil', src:'songs/for you.mp3',           cover:'images/for you.png'          },
   { title:'Chhod De Narazgi',           artist:'Akhil', src:'songs/nara.mp3',           cover:'images/nara.png'          },
   { title:'T-Shirt',           artist:'Akhil', src:'songs/tshirt.mp3',           cover:'images/tshirt.png'          },
